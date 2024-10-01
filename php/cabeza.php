fb5005d96b096d4c3c5d35f140cc4cfbea808ea6
JarisonA34 <jarisonalonzo34@gmail.com>
2024-10-01 10:52:30 -0400

    NOW WITH ICON

--- php/cabeza.php
+++ php/cabeza.php
@@ -7,7 +7,8 @@ include_once("./php/funciones.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity=""> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="">
+    <link rel= icon href="https://cdn-icons-png.flaticon.com/512/1176/1176238.png"
     <title>Calcula</title>
 </head>
 <body class = "d-flex flex-column justify-content-center align-items-center bs-body-bg">
