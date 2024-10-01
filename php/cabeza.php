c01f0e66ae2a580f286d69dbeb06956ba88d6675
JarisonA34 <jarisonalonzo34@gmail.com>
2024-10-01 10:55:37 -0400

    Error fix

--- php/cabeza.php
+++ php/cabeza.php
@@ -8,7 +8,7 @@ include_once("./php/funciones.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="">
-    <link rel= icon href="https://cdn-icons-png.flaticon.com/512/1176/1176238.png"
+    <link rel= icon href="https://cdn-icons-png.flaticon.com/512/1176/1176238.png">
     <title>Calcula</title>
 </head>
 <body class = "d-flex flex-column justify-content-center align-items-center bs-body-bg">
